112ffa7fd7019f8c640909906f2df3f4b18dbc14
devilshadow-max <116060497+devilshadow-max@users.noreply.github.com>
2022-10-25 12:12:21 +0530

    Add files via upload

---
+++ 01_first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// /*
+// This is our first c program
+// which is awesome!
+// */
+int main()
+{
+    //     int tom;
+    //     int Tom;
+    //     // Declaring variables to store error codes
+    //     int error_code;
+    //     // 'J' --> a character
+    //     printf("Hello I am learning C with Harry");
+
+    int number;
+    printf("enter number");
+    scanf("%d", &number);
+    printf("the cube is %d ", number * number * number);
+    return 0;
+}
+
